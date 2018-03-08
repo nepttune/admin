3a5afe895ae28ea3b7270213990a4f84eb0e1cdd
Václav Pelíšek <peldax@gmail.com>
2018-03-08 18:06:03 +0100

    Update AdminRouterFactory.php

--- src/AdminRouterFactory.php
+++ src/AdminRouterFactory.php
@@ -5,29 +5,8 @@ namespace Nepttune;
 use Nette\Application\Routers\RouteList,
     Nette\Application\Routers\Route;
 
-final class AdminRouterFactory extends RouterFactory
+class AdminRouterFactory extends RouterFactory
 {
-    /**
-     * @return \Nette\Application\IRouter
-     */
-    public static function createSubdomainRouter()
-    {
-        $router = parent::createSubdomainRouter();
-
-        $router->prepend(new Route('//admin.%domain%/[<locale [a-z]{2}>/]<presenter>/<action>[/<id>]', [
-            'module' => 'Nepttune',
-            'presenter' => [
-                Route::PATTERN => 'user|role|sign'
-            ],
-            'action' => 'default',
-            'id' => [
-                Route::PATTERN => '\d+'
-            ]
-        ]));
-
-        return $router;
-    }
-
     /**
      * @return \Nette\Application\IRouter
      */
@@ -35,11 +14,9 @@ final class AdminRouterFactory extends RouterFactory
     {
         $router = parent::createStandardRouter();
 
-        $router->prepend(new Route('/admin/<presenter>/<action>[/<id>]', [
-            'module' => 'Nepttune',
-            'presenter' => [
-                Route::PATTERN => 'user|role|sign'
-            ],
+        $router->prepend(new Route('/[<locale [a-z]{2}>/]admin/<presenter>/<action>[/<id>]', [
+            'module' => 'Admin',
+            'presenter' => 'Default',
             'action' => 'default',
             'id' => [
                 Route::PATTERN => '\d+'
