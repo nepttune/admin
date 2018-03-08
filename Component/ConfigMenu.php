7df011fbbd5b6b95ebb0f6678dbf31e2b9fe2656
Václav Pelíšek <peldax@gmail.com>
2018-03-08 01:34:41 +0100

    Add files via upload

---
+++ Component/ConfigMenu.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Nepttune\Component;
+
+final class ConfigMenu extends BaseComponent
+{
+    /** @var array */
+    protected $menu;
+
+    public function __construct(array $menu)
+    {
+        $this->menu = $menu;
+    }
+
+    protected function beforeRender() : void
+    {
+        $this->template->menu = $this->menu;
+    }
+}
---
+++ Presenter/SignPresenter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Nepttune\Presenter;
+
+abstract class SignPresenter extends BasePresenter
+{
+    /** @persistent */
+    public $backlink;
+
+    public function actionOut()
+    {
+        $this->user->logout();
+
+        $this->flashMessage('Successfully logged out.', 'success');
+        $this->redirect($this->context->parameters['redirectSignOut']);
+    }
+
+    public function renderIn()
+    {
+        $this->template->setFile(__DIR__ . '/../templates/Sign/in.latte');
+    }
+}
