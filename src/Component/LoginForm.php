fafc459d428cc2279753ccc16e6f8e072b16401a
Václav Pelíšek <peldax@gmail.com>
2018-03-09 01:33:45 +0100

    Update LoginForm.php

--- src/Component/LoginForm.php
+++ src/Component/LoginForm.php
@@ -8,20 +8,49 @@ final class LoginForm extends BaseFormComponent
 {
     const SAVE_NEXT = false;
     const SAVE_LIST = false;
-    
+
+    /** @var  string */
+    protected $redirectSignIn;
+
     /** @var  \Nepttune\Model\LoginLogModel */
-    protected $logLoginModel;
+    protected $loginLogModel;
+
+    /** @var \Nette\Http\Request */
+    protected $request;
+
+    public function __construct(
+        string $redirectSignIn,
+        \Nepttune\Model\LoginLogModel $loginLogModel,
+        \Nette\Http\Request $request)
+    {
+        $this->redirectSignIn = $redirectSignIn;
+        $this->loginLogModel = $loginLogModel;
+        $this->request = $request;
+    }
 
     protected function modifyForm(Form $form) : Form
     {
         $form->addText('username', 'Username')->setRequired();
         $form->addPassword('password', 'Password')->setRequired();
 
+        $ids = $this->loginLogModel->getTable()
+            ->where('ip_address', inet_pton($this->request->getRemoteAddress()))
+            ->order('id DESC')
+            ->limit(5)
+            ->fetchPairs(null, 'id');
+
+        if ($this->loginLogModel->getTable()->where('id', $ids)->where('result', 'failure')->count() === 5)
+        {
+            $form->addReCaptcha('recaptcha', 'Security check', 'Please prove you are not a robot.');
+        }
+
         return $form;
     }
 
     public function formSuccess(\Nette\Application\UI\Form $form, \stdClass $values) : void
     {
+        $failure = false;
+
         try
         {
             $this->getPresenter()->getUser()->login($values->username, $values->password);
@@ -29,13 +58,24 @@ final class LoginForm extends BaseFormComponent
         }
         catch (\Nette\Security\AuthenticationException $e)
         {
-            $this->getPresenter()->flashMessage($e->getMessage(), 'danger');
-            return;
+            $failure = $e->getMessage();
+        }
+
+        $this->loginLogModel->insert([
+            'datetime' => new \Nette\Utils\DateTime(),
+            'result' => (bool) $failure ? 'failure' : 'success',
+            'ip_address' => inet_pton($this->request->getRemoteAddress()),
+            'username' => $values->username
+        ]);
+
+        if ($failure)
+        {
+            $this->getPresenter()->flashMessage($failure, 'danger');
+            $this->getPresenter()->redirect('this');
         }
 
         $this->getPresenter()->flashMessage('Successfully logged in.', 'success');
-
         $this->getPresenter()->restoreRequest($this->getPresenter()->getParameter('backlink'));
-        $this->getPresenter()->redirect($this->getPresenter()->context->parameters['redirectSignIn']);
+        $this->getPresenter()->redirect($this->redirectSignIn);
     }
 }
