88c634e36903b5fe53b77f7955d5216c42dd31f7
Václav Pelíšek <peldax@gmail.com>
2018-03-10 04:42:16 +0100

    Add files via upload

--- src/Presenter/RolePresenter.php
+++ src/Presenter/RolePresenter.php
@@ -4,5 +4,25 @@ namespace App\AdminModule\Presenter;
 
 final class RolePresenter extends \Nepttune\Presenter\BaseAuthPresenter
 {
+    /**
+     * @inject
+     * @var  \Nepttune\Component\IRoleFormFactory
+     */
+    public $iRoleFormFactory;
 
+    /**
+     * @inject
+     * @var  \Nepttune\Component\IRoleListFactory
+     */
+    public $iRoleListFactory;
+
+    protected function createComponentRoleForm()
+    {
+        return $this->iRoleFormFactory->create();
+    }
+
+    protected function createComponentRoleList()
+    {
+        return $this->iRoleListFactory->create();
+    }
 }
--- src/Presenter/UserPresenter.php
+++ src/Presenter/UserPresenter.php
@@ -4,5 +4,25 @@ namespace App\AdminModule\Presenter;
 
 final class UserPresenter extends \Nepttune\Presenter\BaseAuthPresenter
 {
+    /**
+     * @inject
+     * @var  \Nepttune\Component\IUserFormFactory
+     */
+    public $iUserFormFactory;
 
+    /**
+     * @inject
+     * @var  \Nepttune\Component\IUserListFactory
+     */
+    public $iUserListFactory;
+
+    protected function createComponentUserForm()
+    {
+        return $this->iUserFormFactory->create();
+    }
+
+    protected function createComponentUserList()
+    {
+        return $this->iUserListFactory->create();
+    }
 }
