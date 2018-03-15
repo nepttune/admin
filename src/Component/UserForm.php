dc97cf046ea4ed7358f5571305e7f18708f33a05
Václav Pelíšek <peldax@gmail.com>
2018-03-16 00:06:29 +0100

    Update UserForm.php

--- src/Component/UserForm.php
+++ src/Component/UserForm.php
@@ -17,6 +17,7 @@ final class UserForm extends BaseFormComponent
     protected function modifyForm(Form $form) : Form
     {
         $form->addText('username', 'admin.username')
+            ->addRule([$this, static::VALIDATOR_UNIQUE], static::VALIDATOR_UNIQUE_MSG)
             ->setRequired();
         $form->addPassword('password', 'admin.password')
             ->setRequired();
