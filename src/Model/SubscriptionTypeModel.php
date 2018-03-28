f178653f7d7b441d752c73c999ca7c154f41789f
Václav Pelíšek <peldax@gmail.com>
2018-03-28 09:18:20 +0200

    Add files via upload

---
+++ src/Model/SubscriptionTypeModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+final class SubscriptionTypeModel extends \Nepttune\Model\BaseModel
+{
+    const TABLE_NAME = 'subscription_type';
+}
---
+++ src/Model/UserSubscriptionTypeModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+final class UserSubscriptionTypeModel extends \Nepttune\Model\BaseModel
+{
+    const TABLE_NAME = 'user_subscription_type';
+}
