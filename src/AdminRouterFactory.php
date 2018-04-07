f633d24a5ff0102cffae9f0eed53a95dbedac4c8
Václav Pelíšek <peldax@gmail.com>
2018-04-07 13:56:19 +0200

    Update AdminRouterFactory.php

--- src/AdminRouterFactory.php
+++ src/AdminRouterFactory.php
@@ -7,21 +7,50 @@ use Nette\Application\Routers\RouteList,
 
 class AdminRouterFactory extends RouterFactory
 {
-    /**
-     * @return \Nette\Application\IRouter
-     */
-    public static function createStandardRouter() : \Nette\Application\IRouter
+    public static function prependDefault(RouteList $router, string $base)
+    {
+        $router->prepend(new Route($base . 'role/<action>[/<id>]', [
+            'locale' => [Route::PATTERN => '[a-z]{2}'],
+            'presenter' => 'Role',
+            'action' => 'default',
+            'id' => [Route::PATTERN => '\d+']
+        ]));
+
+        $router->prepend(new Route($base . 'user/<action>[/<id>]', [
+            'locale' => [Route::PATTERN => '[a-z]{2}'],
+            'presenter' => 'User',
+            'action' => 'default'
+        ]));
+
+        $router->prepend(new Route($base . 'sign/<action>', [
+            'locale' => [Route::PATTERN => '[a-z]{2}'],
+            'presenter' => 'Sign',
+            'action' => 'default',
+            'id' => [Route::PATTERN => '\d+']
+        ]));
+    }
+
+    public static function createSubdomainRouter(string $module = 'admin') : RouteList
+    {
+        $router = parent::createSubdomainRouter();
+
+        static::prependDefault($router, "//{$module}.%domain%/[<locale>/]");
+
+        return $router;
+    }
+
+    public static function createStandardRouter(string $module = 'admin') : RouteList
     {
         $router = parent::createStandardRouter();
 
-        $router->prepend(new Route('/[<locale [a-z]{2}>/]admin/<presenter>/<action>[/<id>]', [
-            'module' => 'Admin',
+        $router->prepend(new Route("/[<locale>/]{$module}/<presenter>/<action>[/<id>]", [
+            'locale' => [Route::PATTERN => '[a-z]{2}'],
             'presenter' => 'Default',
             'action' => 'default',
-            'id' => [
-                Route::PATTERN => '\d+'
-            ]
+            'id' => [Route::PATTERN => '\d+']
         ]));
+
+        static::prependDefault($router, "/[<locale>/]{$module}/");
 
         return $router;
     }
