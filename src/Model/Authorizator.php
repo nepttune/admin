6a6ee158cbae773aaf8f0e7a9c23be5760513f41
Václav Pelíšek <peldax@gmail.com>
2018-04-14 14:14:27 +0200

    Create Authorizator.php

---
+++ src/Model/Authorizator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types = 1);
+
+namespace Nepttune\Model;
+
+final class Authorizator
+{
+    use \Nette\SmartObject;
+
+    /** @var  UserAccessModel */
+    private $userAccessModel;
+
+    public function __construct(UserAccessModel $userAccessModel)
+    {
+        $this->userAccessModel = $userAccessModel;
+    }
+
+    public function isAllowed($userId, $resource, $privilege) : bool
+    {
+        return $this->userAccessModel->findByArray([
+            'user_id' => $userId,
+            'resource' => $resource,
+            'privilege' => $privilege
+        ])->count() > 0;
+    }
+}
