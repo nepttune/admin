9b462f708ff614865723251b2b7fab795205b7b7
Václav Pelíšek <peldax@gmail.com>
2018-04-18 16:15:00 +0200

    Update UserList.php

--- src/Component/UserList.php
+++ src/Component/UserList.php
@@ -20,6 +20,8 @@ final class UserList extends BaseListComponent
 {
     public function __construct(\Nepttune\Model\UserModel $userModel)
     {
+        parent::__construct();
+        
         $this->repository = $userModel;
     }
 
