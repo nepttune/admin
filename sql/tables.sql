48ea0011bbed788fe6be6721aa6c704098c72c47
Václav Pelíšek <peldax@gmail.com>
2018-04-23 16:57:20 +0200

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -61,3 +61,6 @@ CREATE TABLE IF NOT EXISTS `user_subscription_type`
 ALTER TABLE `subscription`
   ADD CONSTRAINT `subscription_user_id_fk` 
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+  
+INSERT INTO `user` (`id`, `username`, `password`, `registered`, `active`) VALUES
+(1, 'test', '$2y$10$yX2aYVewjkhJywP8QIpyvOtFqr8xYIIAh4fIwZkP67DPVKk7WCt6.', '2018-04-23', 1);
