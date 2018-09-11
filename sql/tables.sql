afe52b838e64d0de677479851e70a50948ec7987
Václav Pelíšek <peldax@gmail.com>
2018-09-11 15:27:55 +0200

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -17,30 +17,6 @@ CREATE TABLE IF NOT EXISTS `log_login`
   INDEX `log_login_ip_address_index` (`ip_address`)
 ) ENGINE = INNODB;
 
-CREATE TABLE IF NOT EXISTS `user`
-(
-  `id`         INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `username`   VARCHAR(255)                NOT NULL,
-  `password`   VARCHAR(255)                NOT NULL,
-  `registered` DATE                        NOT NULL,
-  `root`       TINYINT DEFAULT 0           NOT NULL,
-  `active`     TINYINT DEFAULT 1           NOT NULL,
-
-  INDEX `user_active_username_index` (`active`, `username`)
-) ENGINE = INNODB;
-
-CREATE TABLE IF NOT EXISTS `user_access`
-(
-  `id`         INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `user_id`    INT(10) UNSIGNED            NOT NULL,
-  `resource`   VARCHAR(255)                NOT NULL,
-
-  CONSTRAINT `user_access_user_id_fk`
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-
-  INDEX `user_access_user_id_resource_index` (`user_id`, `resource`)
-) ENGINE = INNODB;
-
 CREATE TABLE IF NOT EXISTS `role`
 (
   `id`        INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -58,6 +34,21 @@ CREATE TABLE IF NOT EXISTS `role_access`
 
   CONSTRAINT `role_access_role_id_fk`
   FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+  `id`         INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `role_id`    INT(10) UNSIGNED            NOT NULL,
+  `username`   VARCHAR(255)                NOT NULL,
+  `password`   VARCHAR(255)                NOT NULL,
+  `registered` DATE                        NOT NULL,
+  `root`       TINYINT DEFAULT 0           NOT NULL,
+  `active`     TINYINT DEFAULT 1           NOT NULL,
+
+  CONSTRAINT `user_role_id_fk`
+  FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  INDEX `user_active_username_index` (`active`, `username`)
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `subscription_type` (
